6e051222f8299bad7d3c4217f09fffe24510f4ba
Timothy Wamalwa <timwamalwa@gmail.com>
2022-10-17 08:52:53 +0300

    Added test scripts

---
+++ tools/Test.ps1
@@ -0,0 +1,13 @@
+$Date = Get-Date -Format "dd-MM-yyyy"
+$ProposedBranch = "WeeklyExamplesUpdate/"+$Date
+$Exists = git branch -l $ProposedBranch
+if ([string]::IsNullOrEmpty($Exists)) {
+    git checkout -b $ProposedBranch
+}else{
+	Write-Host "Branch already exists"
+    $CurrentBranch = git rev-parse --abbrev-ref HEAD
+    if($CurrentBranch -ne $ProposedBranch){
+        git checkout $ProposedBranch
+     }
+     git checkout $ProposedBranch
+}
